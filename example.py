4e2f7a065621c55cf9aad72fdb30de27ba27f69e
Pinky Chauhan <pinky@pinkys-mbp.lan>
2020-09-12 15:45:26 -0500

    MP1

--- example.py
+++ example.py
@@ -1,11 +1,18 @@
 import metapy
 
 def tokens_lowercase(doc):
-    #Write a token stream that tokenizes with ICUTokenizer, 
+    #Write a token stream that tokenizes with ICUTokenizer,
     #lowercases, removes words with less than 2 and more than 5  characters
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
     '''Place your code here'''
-    
+    tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+    tok = metapy.analyzers.LowercaseFilter(tok)
+    tok = metapy.analyzers.LengthFilter(tok, min=2, max=5)
+    tok = metapy.analyzers.Porter2Filter(tok)
+    tok.set_content(doc.content())
+    ana = metapy.analyzers.NGramWordAnalyzer(3, tok)
+    trigrams = ana.analyze(doc)
+        
     #leave the rest of the code as is
     tok.set_content(doc.content())
     tokens, counts = [], []
@@ -13,7 +20,7 @@ def tokens_lowercase(doc):
         counts.append(count)
         tokens.append(token)
     return tokens
-    
+
 if __name__ == '__main__':
     doc = metapy.index.Document()
     doc.content("I said that I can't believe that it only costs $19.95! I could only find it for more than $30 before.")
